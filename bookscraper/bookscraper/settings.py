0166589f53f7717969ee9b7d38ba254ae8150b5d
Luca Di Felice <ldifelice06@gmail.com>
2025-05-14 13:33:53 -0400

    Add ScrapeOpsFakeUserAgentMiddleware to rotate user agents and update settings (fixed it)

--- bookscraper/bookscraper/settings.py
+++ bookscraper/bookscraper/settings.py
@@ -16,6 +16,11 @@ FEEDS = {
     "data.json" : {"format": "json"}
 }
 
+SCRAPEOPS_API_KEY = "abc05925-98e1-4cfd-9b64-aa5adcf8f98f"
+SCRAPEOPS_FAKE_USER_AGENT_ENDPOINT = "https://headers.scrapeops.io/v1/user-agents"
+SCRAPEOPS_FAKE_USER_AGENT_ENABLED = True
+SCRAPEOPS_NUM_RESULTS = 50
+
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'bookscraper (+http://www.yourdomain.com)'
 
